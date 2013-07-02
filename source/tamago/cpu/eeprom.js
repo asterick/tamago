17184dc5b0571269c888b5fce2190baf86547683
Bryon Vandiver <unicdk@gmail.com>
2013-07-01 20:12:52 -0700

    Reenable EEPROM on boot

--- source/tamago/cpu/eeprom.js
+++ source/tamago/cpu/eeprom.js
@@ -31,7 +31,7 @@ module.exports = (function(){
 		this._last_data = data;
 
 		// This chip is not receiving power, so it is idle.
-		if (true || !power) {
+		if (!power) {
 			this._state = DISABLED;
 			this._output = 1; // NACK
 			return ;
